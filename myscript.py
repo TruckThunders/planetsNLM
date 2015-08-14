0a80a20c95e252338d5846db3c66476893327727
TruckThunders <nigelmathes@gmail.com>
2015-08-14 09:38:39 -0600

    I made a Haiku, thoroughly discussed New Horizons, and created a python script

---
+++ myscript.py
@@ -0,0 +1,13 @@
+# PYTHON CODE TO DO STUFF FOR SOFTWARE CARPENTRY
+# THINGS TO DO GO HERE
+
+import numpy as np
+import matplotlib.pyplot as plt
+np.set_printoptions(threshold=np.nan)
+
+try:
+
+    print "Beginning script."
+
+except Exception, e:
+    print 'You broke it. Good job. Now try again, but this time with feeling.'
